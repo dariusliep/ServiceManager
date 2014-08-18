3797a3d7a52e17ad973e3aa1f9f1b2e954aea287
Darius L <droad27@gmail.com>
2014-08-18 21:02:35 +0300

    Forgot to add files

---
+++ ServicesOperations/Service.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicesOperations
+{
+    public class Service
+    {
+        private string _name;
+        private string _status;
+        private string _displayName;
+
+        public string DisplayName
+        {
+            get { return _displayName; }
+            set { _displayName = value; }
+        }
+
+        public string Name
+        {
+            set { _name = value; }
+            get { return _name; }
+        }
+
+        public string Status
+        {
+            set { _status = value; }
+            get { return _status; }
+        }
+
+        public Service()
+        {
+
+        }
+
+        public Service(ServiceController sc)
+        {
+            Status = ServicesReading.GetServiceStatusString(sc.Status);
+            Name = sc.ServiceName;
+            DisplayName = sc.DisplayName;
+        }
+    }
+}
---
+++ ServicesOperations/ServicesReading.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceProcess;
+
+namespace ServicesOperations
+{
+    public class ServicesReading
+    {
+        public List<Service> ReadAllServices()
+        {
+            // reading all services
+            ServiceController[] services = ServiceController.GetServices();
+
+            List<Service> servicesList = new List<Service>();
+
+            foreach(ServiceController service in services)
+            {
+                Service tempService = new Service(service);
+                servicesList.Add(tempService);
+            }
+            return servicesList;
+        }
+
+        public static string GetServiceStatusString(ServiceControllerStatus status)
+        {
+            try 
+	        {
+                switch (status)
+                {
+                    case ServiceControllerStatus.ContinuePending:
+                        return "Continue pending";
+                    case ServiceControllerStatus.Paused:
+                        return "Paused";
+                    case ServiceControllerStatus.PausePending:
+                        return "Pause pending";
+                    case ServiceControllerStatus.Running:
+                        return "Running";
+                    case ServiceControllerStatus.StartPending:
+                        return "Start pending";
+                    case ServiceControllerStatus.Stopped:
+                        return "Stopped";
+                    case ServiceControllerStatus.StopPending:
+                        return "Stop pending";
+                    default:
+                        return "unknown";
+                }
+	        }
+	        catch (Exception)
+	        {
+		        throw;
+	        }        
+        }
+    }
+}
